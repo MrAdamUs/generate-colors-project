8c9c1338527c09f4320618afd560558c6982f740
MrAdam <mradam@MrAdams-MacBook-Pro.local>
2021-01-02 08:29:05 -0600

    start working on js file setup chroma and add color to the divs and scaleSet

---
+++ app.js
@@ -0,0 +1,75 @@
+// Global selections and varibale
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+
+// functions
+
+// Color Generater without chroma js
+// function generateHex() {
+//   const letters = '#0123456789ABCDEF';
+//   let hash = '#';
+//   for (let i = 0; i < 6; i++) {
+//     hash += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return hash;
+// }
+
+// with chroma js
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    // Add color to the backGorund
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    // check for contrast
+    checkTextContrast(randomColor, hexText);
+    // Initial Colorize Sliders
+
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const stauration = sliders[2];
+
+    colorizeSliders(color, hue, brightness, stauration);
+  });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = '#000';
+  } else {
+    text.style.color = '#fff';
+  }
+}
+
+function colorizeSliders(color, hue, brightness, stauration) {
+  //Scale Saturation
+  const noSat = color.set('hsl.s', 0);
+  const fullSat = color.set('hsl.s', 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //Scale brightness
+  const midBright = color.set('hsl.l', 0.5);
+  const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+  //Input update
+  stauration.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0,
+  )},${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0,
+  )},${scaleBright(0.5)},${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+randomColors();
